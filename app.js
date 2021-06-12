428b31cae42c9ee677f8ee9caa27f41b49fdccda
jian-level-is <myunggeun22@gmail.com>
2021-06-12 19:42:26 +0900

    Update app.js
    about
    - const , let , var
    - string, float ,,,
    - function name(), function(){ ... }

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-alert("hi");
+const a = 5;
+let b= 5;
+console.log(5+5);
+
+const dayOfWeek = ["mon","tue","wed","thu","fri"];
+console.log(dayOfWeek);
+
+//Get Item from Array
+console.log(dayOfWeek[3]);
+
+//Add one more day to the array
+dayOfWeek.push("sat");
+console.log(dayOfWeek);
+
+const player ={
+    name: "sae",
+    points : 10,
+    fat : false
+};
+
+console.log(player);
+console.log(player.fat);
+console.log(player["name"]);
+console.log(player.name);
+
+function sayHello(){
+    console.log("hello!");
+}
+
+sayHello();
