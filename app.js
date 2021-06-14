ea112df1fc46ce3c172ac9695f9ef230431097e0
jian-level-is <myunggeun22@gmail.com>
2021-06-14 23:19:32 +0900

    Update app.js
    about,
    className, classList
    contains, remove, add
    toggle is super powerful!

--- app.js
+++ app.js
@@ -1,50 +1,80 @@
-const a = 5;
-let b= 5;
-console.log(5+5);
+// const a = 5;
+// let b= 5;
+// console.log(5+5);
 
-const dayOfWeek = ["mon","tue","wed","thu","fri"];
-console.log(dayOfWeek);
+// const dayOfWeek = ["mon","tue","wed","thu","fri"];
+// console.log(dayOfWeek);
 
-//Get Item from Array
-console.log(dayOfWeek[3]);
+// //Get Item from Array
+// console.log(dayOfWeek[3]);
 
-//Add one more day to the array
-dayOfWeek.push("sat");
-console.log(dayOfWeek);
+// //Add one more day to the array
+// dayOfWeek.push("sat");
+// console.log(dayOfWeek);
 
-const player ={
-    name: "sae",
-    points : 10,
-    fat : false
-};
+// const player ={
+//     name: "sae",
+//     points : 10,
+//     fat : false
+// };
 
-console.log(player);
-console.log(player.fat);
-console.log(player["name"]);
-console.log(player.name);
+// console.log(player);
+// console.log(player.fat);
+// console.log(player["name"]);
+// console.log(player.name);
 
-function sayHello(){
-    console.log("hello!");
+// function sayHello(){
+//     console.log("hello!");
+// }
+
+// sayHello();
+
+// const coculator = {
+//     add : function(a , b){
+//         console.log(a+b);
+//     },
+//     minus : function(a , b){
+//         console.log(a-b);
+//     },
+//     division : function(a , b){
+//         console.log(a/b);
+//     },
+//     fowerOf : function(a,b){
+//         console.log(a**b);
+//     }
+// }
+
+// coculator.add(1,5);
+// coculator.minus(5,12);
+// coculator.division(5,2);
+// coculator.fowerOf(5,2);
+
+const title = document.querySelector("div h1");
+//1 way
+function titleClick1(){
+    let newColor;
+    if(title.style.color === "blue"){
+        newColor = "orange";
+    }else{
+        newColor = "blue";
+    }
+    title.style.color = newColor;
 }
 
-sayHello();
-
-const coculator = {
-    add : function(a , b){
-        console.log(a+b);
-    },
-    minus : function(a , b){
-        console.log(a-b);
-    },
-    division : function(a , b){
-        console.log(a/b);
-    },
-    fowerOf : function(a,b){
-        console.log(a**b);
+//2wqy 
+function titleClick2(){
+    const clickedClass = "clicked";
+    if(title.classList.contains(clickedClass)){
+        title.classList.remove(clickedClass);
+    }else{
+        title.classList.add(clickedClass);
     }
 }
 
-coculator.add(1,5);
-coculator.minus(5,12);
-coculator.division(5,2);
-coculator.fowerOf(5,2);
+//best way 3
+function titleClick3(){
+    title.classList.toggle("clicked");
+}
+
+
+title.addEventListener("click", titleClick);
