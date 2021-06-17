4b8e0b1eb2c52222da40ce4a319566245f26ea26
jian-level-is <myunggeun22@gmail.com>
2021-06-18 00:59:32 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,13 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos";
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
@@ -11,6 +18,7 @@ function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
     span.innerText = newTodo;
+    
 
     const button = document.createElement("button");
     button.innerText = "X";
@@ -25,7 +33,16 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value; 
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parseToDos = JSON.parse(savedToDos);
+    parseToDos.forEach((item) => console.log("hello", item));
+}
