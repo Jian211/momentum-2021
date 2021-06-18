d83f1cdccccdadb3c31a15ac5ca05f69a56ca348
jian-level-is <myunggeun22@gmail.com>
2021-06-18 20:27:41 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -3,22 +3,29 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
 
 const TODOS_KEY = "todos";
-const toDos = [];
+let toDos = [];
 
+// todo list를 브라우저에 저장한다. 
 function saveToDos(){
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
+// TOdo list삭제 
 function deleteToDo(event) {
+    // event가 있었던 개체의 부모 엘리먼트를 받아서 변수에 저장
     const li = event.target.parentElement;
+    // 저장 후 삭제
     li.remove();
+    toDos = toDos.filter((todo) => todo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintToDo(newTodo){
     const li = document.createElement("li");
-    const span = document.createElement("span");
-    span.innerText = newTodo;
+    li.id = newTodo.id;
     
+    const span = document.createElement("span");
+    span.innerText = newTodo.text;
 
     const button = document.createElement("button");
     button.innerText = "X";
@@ -33,8 +40,13 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value; 
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+
+    const newToDoObj = {
+        "text" : newTodo,
+        "id" : Date.now(), 
+    };
+    toDos.push(newToDoObj);
+    paintToDo(newToDoObj);
     saveToDos();
 }
 
@@ -44,5 +56,6 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if(savedToDos !== null){
     const parseToDos = JSON.parse(savedToDos);
-    parseToDos.forEach((item) => console.log("hello", item));
+    toDos = parseToDos;
+    parseToDos.forEach(paintToDo);
 }
